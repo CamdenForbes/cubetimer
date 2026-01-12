99fab89089eeee0d31112d5b292c6a4503610eab
Camden <135179284+CamdenForbes@users.noreply.github.com>
2026-01-11 19:10:13 -0800

    Implement main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "scramblegen.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("SCRAMBLE: ");
+
+  char scramble[30][2];
+  genScramble(scramble);
+
+  for (int i = 0; i < 30; i++) {
+    printf("[");
+    for (int j = 0; j < 2; j++) {
+      printf("%c", scramble[i][j]);
+    }
+    printf("]");
+  }
+
+  return 0;
+}
