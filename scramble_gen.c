db8484be7706259ea031f233000cb07aeffaddc5
Camden <135179284+CamdenForbes@users.noreply.github.com>
2026-01-12 21:08:57 -0800

    Improve scramble generation logic
    Refactor scramble generation logic to prevent cancelling moves and back-to-back opposite rotations.

--- scramble_gen.c
+++ scramble_gen.c
@@ -9,12 +9,11 @@ void genMoves(char (*scramble)[2]) {
                       "D ", "D'", "B ", "B'", "F ", "F'"};
   srand(time(NULL));
   int prev_i = rand() % 12;
+  int prev_prev_i = rand() % 12;
   int rand_i;
-  for (int i = 0; i < 30; i++) {
+  for (int i = 0; i < 20; i++) {
     rand_i = rand() % 12;
-    while ((rand_i % 2 == 1 && rand_i == prev_i + 1) ||
-           (rand_i % 2 == 0 &&
-            rand_i == prev_i - 1)) { // prevent moves that cancel
+    while (cleanScramble(prev_prev_i, prev_i, rand_i)) {
       rand_i = rand() % 12;
     }
 
@@ -23,6 +22,29 @@ void genMoves(char (*scramble)[2]) {
   }
 }
 
-void cleanScramble(char (*scramble)[2]) {}
+bool cleanScramble(int prev_prev_i, int prev_i, int rand_i) {
+  char *notation[] = {"R ", "R'", "L ", "L'", "U ", "U'",
+                      "D ", "D'", "B ", "B'", "F ", "F'"};
+  // Prevent Cancelling Moves
+  if ((rand_i % 2 == 1 && rand_i == prev_i + 1) ||
+      (rand_i % 2 == 0 && rand_i == prev_i - 1)) {
+    return true;
+  }
+  // Prevent Moves That Convert to '
+  if (prev_prev_i == prev_i && prev_i == rand_i) {
 
+    return true;
+  }
+  // Prevent Back To Back Opposite Rotation
+  if ((rand_i == 0 || rand_i == 1 || rand_i == 4 || rand_i == 5 ||
+       rand_i == 8 || rand_i == 9) &&
+      (prev_i == rand_i + 2)) {
+    return true;
+  } else if ((rand_i == 2 || rand_i == 3 || rand_i == 6 || rand_i == 7 ||
+              rand_i == 10 || rand_i == 11) &&
+             (prev_i == rand_i - 2)) {
+    return true;
+  }
+  return false;
+}
 void genScramble(char (*scramble)[2]) { genMoves(scramble); }
