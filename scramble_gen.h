68ee5dc10eb4f4fa9d413998599a83ce740d895d
Camden <135179284+CamdenForbes@users.noreply.github.com>
2026-01-12 21:08:30 -0800

    Refactor cleanScramble function signature

--- scramble_gen.h
+++ scramble_gen.h
@@ -1,8 +1,8 @@
 #ifndef SCRAMBLE_GEN
 #define SCRAMBLE_GEN
-
+#include <stdbool.h>
 void genScramble(char (*arr)[2]);
-void cleanScramble(char (*arr)[2]);
+bool cleanScramble(int prev_prev_i, int prev_i, int rand_i);
 void genMoves(char (*arr)[2]);
 
 #endif
