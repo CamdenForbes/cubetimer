42f9b272bbe84705436165526b0690858ca1c165
CamdenForbes <camdendforbes@gmail.com>
2026-01-15 17:39:22 -0800

    cleaned random state logic

--- scramble_gen.c
+++ scramble_gen.c
@@ -4,47 +4,26 @@
 #include <string.h>
 #include <time.h>
 
-void genMoves(char (*scramble)[2]) {
-  char *notation[] = {"R ", "R'", "L ", "L'", "U ", "U'",
-                      "D ", "D'", "B ", "B'", "F ", "F'"};
+void genRandomState(char (*scramble)[2]) {
+  char *notation[] = {"R ", "L ", "U ", "D ", "F ", "B "};
+  char postfix[] = {' ', '2', '\''};
   srand(time(NULL));
-  int prev_i = rand() % 12;
-  int prev_prev_i = rand() % 12;
-  int rand_i;
-  for (int i = 0; i < 20; i++) {
-    rand_i = rand() % 12;
-    while (cleanScramble(prev_prev_i, prev_i, rand_i)) {
-      rand_i = rand() % 12;
-    }
 
+  int rand_i = rand() % 6;
+  int prev_i = rand() % 6;
+  for (int i = 0; i < 20; i++) {
+    while (prev_i == rand_i || prev_i % 2 == 0 && rand_i == prev_i + 1 ||
+           prev_i % 2 != 0 && rand_i == prev_i - 1) {
+      rand_i = rand() % 6;
+    }
     strcpy(scramble[i], notation[rand_i]);
     prev_i = rand_i;
   }
+
+  for (int i = 0; i < 20; i++) {
+    rand_i = rand() % 3;
+    scramble[i][1] = postfix[rand_i];
+  }
 }
 
-bool cleanScramble(int prev_prev_i, int prev_i, int rand_i) {
-  char *notation[] = {"R ", "R'", "L ", "L'", "U ", "U'",
-                      "D ", "D'", "B ", "B'", "F ", "F'"};
-  // Prevent Cancelling Moves
-  if ((rand_i % 2 == 1 && rand_i == prev_i + 1) ||
-      (rand_i % 2 == 0 && rand_i == prev_i - 1)) {
-    return true;
-  }
-  // Prevent Moves That Convert to '
-  if (prev_prev_i == prev_i && prev_i == rand_i) {
-
-    return true;
-  }
-  // Prevent Back To Back Opposite Rotation
-  if ((rand_i == 0 || rand_i == 1 || rand_i == 4 || rand_i == 5 ||
-       rand_i == 8 || rand_i == 9) &&
-      (prev_i == rand_i + 2)) {
-    return true;
-  } else if ((rand_i == 2 || rand_i == 3 || rand_i == 6 || rand_i == 7 ||
-              rand_i == 10 || rand_i == 11) &&
-             (prev_i == rand_i - 2)) {
-    return true;
-  }
-  return false;
-}
-void genScramble(char (*scramble)[2]) { genMoves(scramble); }
+void genScramble(char (*scramble)[2]) { genRandomState(scramble); }
--- scramble_gen.h
+++ scramble_gen.h
@@ -1,8 +1,6 @@
 #ifndef SCRAMBLE_GEN
 #define SCRAMBLE_GEN
 #include <stdbool.h>
+void genRandomState(char (*arr)[2]);
 void genScramble(char (*arr)[2]);
-bool cleanScramble(int prev_prev_i, int prev_i, int rand_i);
-void genMoves(char (*arr)[2]);
-
 #endif
